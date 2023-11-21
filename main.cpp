33f407823c09ee3e00713bee29c3079dae595de6
Jason Moss <jmoss35@ut.utm.edu>
2023-11-21 16:57:40 -0600

    Finished

--- main.cpp
+++ main.cpp
@@ -19,48 +19,60 @@ int main() {
   BST tree;
 
   int option = 0;
-  while(option < 7){
+  while(option < 8){
     cout << "Please select an option" << endl;
-    cout << "1. Create a Tree from a file" << endl;
-    cout << "2. Find where the value of a word in the tree" << endl;
-    cout << "3. Print the tree" << endl;
-    cout << "4. Find the smallest value in the tree" << endl;
-    cout << "5. Find the largest value in the tree" << endl;
-    cout << "6. Remove a word from the tree" << endl;
-    cout << "7. Exit" << endl;
+    cout << "1. Add words yourself" << endl;
+    cout << "2. Create a Tree from a file" << endl;
+    cout << "3. Find where the value of a word in the tree" << endl;
+    cout << "4. Print the tree" << endl;
+    cout << "5. Find the smallest value in the tree" << endl;
+    cout << "6. Find the largest value in the tree" << endl;
+    cout << "7. Remove a word from the tree" << endl;
+    cout << "8. Exit" << endl;
 
     cin >> option;
     cout << endl;
 
     if(option == 1){
+      string word;
+      cout << "Please enter word: ";
+      cin >> word;
+      cout << endl << "Please enter number:";
+      int num = 0;
+      cin >> num;
+      cout << endl;
+      tree.set(word, num);
+    }
+
+    if(option == 2){
       string file;
       cout << "Please enter file name: ";
       cin >> file;
       cout << endl;
       tree.save_file(file);
     }
-    if(option == 2){
+    if(option == 3){
       string word;
       cout << "Please enter a word: ";
       cin >> word;
       cout << endl;
       tree.find(word);
     }
-    if(option == 3){
+    if(option == 4){
       cout << "Printing..." << endl;
       tree.print();
     }
-    if(option == 4){
+    if(option == 5){
       cout << "Finding smallest...: ";
       tree.min();
       cout << endl;
     }
-    if(option == 5){
+    if(option == 6){
       cout << "Finding largest...: ";
       tree.max();
       cout << endl;
     }
-    if(option == 6){
+    if(option == 7){
       cout << "Please enter a word to remove: ";
       string word;
       cin >> word;
